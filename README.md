6335707faf19c829463e40a65b316008897caaa0
primerlee <jxlizhibo@gmail.com>
2018-06-21 16:30:26 +0800

    Initial commit

---
+++ README.md
@@ -0,0 +1,1 @@
+# doubanMove-newsReader
---
+++ data/posts-data.js
@@ -0,0 +1,121 @@
+var local_database = [
+    {
+        date: "June 15 2018",
+				title: "国家广播电视总局批准中国移动开展IPTV传输服务",
+        imgSrc: "/images/post/China Mobile.jpeg",
+        avatar: "/images/avatar/1.png",
+				content: "中国移动官微消息称，国家广播电视总局批准中国移动通信集团有限公司开展IPTV传输服务，即日起中国移动用户可以依照国家广播电视总局的相关规定，通过IPTV体验直播电视、时移电视和视频点播等服务。",
+        reading: "112",
+        collection: "96",
+				headImgSrc: "/images/post/China Mobile.jpeg",
+        author: "LU",
+        dateTime: "24小时前",
+				detail: "中国移动官微消息称，国家广播电视总局批准中国移动通信集团有限公司开展IPTV传输服务，即日起中国移动用户可以依照国家广播电视总局的相关规定，通过IPTV体验直播电视、时移电视和视频点播等服务。\n去年6月份，国家新闻出版广电总局曾责令中国联通、中国移动停止违规的IPTV业务，并对已开展的IPTV传输服务业务进行全面整改。",
+        postId: 0,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100003507bR0gDKBm.m4a?fromtag=38",
+            title: "夜夜夜夜-齐秦",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001TEc6V0kjpVC.jpg?max_age=2592000"
+        }
+    },
+    {
+				title: "ofo用户以后需购95元福利包才可继续免押金骑行",
+				content: "引述澎湃新闻的消息，共享单车品牌ofo进一步取消了在上海、杭州、广州、深圳、厦门五个城市的信用免押。ofo方面表示，这并不是意味着变向取消免押金服务，应该是在尝试建立自己的信用体系，这次调整是其中一步",
+        imgSrc: "/images/post/OFO.jpeg",
+        reading: 62,
+				detail: "引述澎湃新闻的消息，共享单车品牌ofo进一步取消了在上海、杭州、广州、深圳、厦门五个城市的信用免押。ofo方面表示，这并不是意味着变向取消免押金服务，应该是在尝试建立自己的信用体系，这次调整是其中一步。\n原本的信用免押金政策取消后，ofo方面表示，信用免押金城市（原25个城市）用户，可购买95元“福利包”，享受全国免押金骑行服务。或可直接缴纳199元押金使用ofo小黄车。95元为账户余额，可用于购买月卡，骑行付费等，不可退。\n5月31日，ofo在20城市取消信用免押金政策，仅沪杭广深厦五城保留。在此之前，ofo的信用免押城市为25个，包括了武汉、石家庄、福州、长沙、银川、中山、佛山、乐山、宁波、芜湖、南宁、上海、杭州、广州、深圳、厦门、南昌、威海、金华、郑州、合肥、温州、绵阳、珠海和德阳。\n6月初有网络媒体称，ofo由于资金链紧张，总部已经开始大规模裁员，同时高管层变动剧烈。随后，ofo方面回应称是“无稽之谈”“背后还有人推动”。\n近日，ofo方面对外宣称，“ofoB2B各项业务进展顺利，目前营收已经超过1亿元。同时ofo在国内100余座城市也已实现盈利。”",
+        collection: 92,
+        dateTime: "24小时前",
+				headImgSrc: "/images/post/OFO.jpeg",
+        author: "LU",
+        date: "June 15 2018",
+        avatar: "/images/avatar/1.png",
+        postId: 1,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100003GdCmG4NkEOR.m4a?fromtag=38",
+            title: "鬼迷心窍-李宗盛",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000002xOmp62kqSic.jpg?max_age=2592000"
+        }
+    },
+    {
+        //按住alt + shift + F 可以格式化代码样式
+				title: "Nokia 7 Plus被强行从Android P降级后多个应用程序失去响应",
+				content: "在上个月的 I/O 2018 开发者大会上，Google 宣布HMD Global 经营的 Nokia 7 Plus可以率先体验到Android P预览版。然而升级到预览版本后，用户在打上了Google六月安全补丁之后，却发现 Android 版本被降级到了 Oreo 。实际上，更新包的大小为 1.3GB，意味着它不仅仅是一个安全更新。",
+				detail: "在上个月的 I/O 2018 开发者大会上，Google 宣布HMD Global 经营的 Nokia 7 Plus可以率先体验到Android P预览版。然而升级到预览版本后，用户在打上了Google六月安全补丁之后，却发现 Android 版本被降级到了 Oreo 。实际上，更新包的大小为 1.3GB，意味着它不仅仅是一个安全更新。\n被强行降级之后，用户遭遇了大量的“应用程序已失去响应”和崩溃，毕竟设备上的数据本应该是在 Android P 全新预览版上使用的 。\n更郁闷的是，大家无法使用诺基亚 Android P 网页上提供的安装包去回滚，因为它比当前使用的版本要旧很多。\n诺基亚没有对这一情况发表过多评论，某位社区论坛版主表示，HMD Global 将对此事展开调查。",
+        imgSrc: "/images/post/Nokia.jpeg",
+				headImgSrc: "/images/post/Nokia.jpeg",
+        reading: 62,
+        collection: 92,
+        author: "LU",
+        date: "June 15 2018",
+        dateTime: "三天前",
+        avatar: "/images/avatar/3.png",
+        postId: 2,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100004HLusI2lLjZy.m4a?fromtag=38",
+            title: "女儿情-万晓利",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004Wv5BO30pPc0.jpg?max_age=2592000"
+        }
+    },
+    {
+				title: "5G首个国际标准正式发布",
+				content: "引述it之家的消息，3GPP全会（TSG#80）批准了第五代移动通信技术标准（5G NR）独立组网功能冻结。至此，5G已经完成第一阶段全功能标准化工作，也意味着首个完整的5G国际标准已正式发布。",
+        imgSrc: "/images/post/5G.png",
+        reading: 102,
+				detail: "引述it之家的消息，3GPP全会（TSG#80）批准了第五代移动通信技术标准（5G NR）独立组网功能冻结。至此，5G已经完成第一阶段全功能标准化工作，也意味着首个完整的5G国际标准已正式发布。\n此前，工信部也已经发布了征求意见稿，要求对相关技术指标和管理规范进行规定。根据工信部的方案，中国的5G承载频段是Sub6GHz的中频。中国工业和信息化部发布通知，正式宣布规划3300-3600MHz、4800-5000MHz频段作为5G系统的工作频段，其中3300-3400MHz频段原则上限室内使用。\n按照3GPP规划，5G标准分为NSA和SA（Standalone独立组网）两种。其中5GNSA组网是一种过渡方案，主要以提升热点区域带宽为主要目标，没有独立信令面，依托4G基站和核心网工作，相对标准制定进展快些。在SA第一阶段的5G标准落定后，第二阶段启动R16为5G标准，于2019年12月完成，届时整个5G标准也就出炉了，同时5G手机将在2019年推出。",
+        collection: 92,
+        dateTime: "24小时前",
+        headImgSrc: "/images/post/5G.png",
+        author: "LU",
+        date: "June 14 2018",
+        avatar: "/images/avatar/5.png",
+        postId: 3,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100002mWVx72p8Ugp.m4a?fromtag=38",
+            title: "恋恋风尘-老狼",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000001VaXQX1Z1Imq.jpg?max_age=2592000",
+        }
+    },
+    {
+			title: "美法院批准 AT&T 以 850 亿美元收购时代华纳",
+			content: "美国联邦法院法官批准 AT&T 收购时代华纳，驳回去年11月司法部提出的此项收购可能涉嫌垄断的诉讼。之后，AT&T将以850亿美元的价格收购时代华纳，这也意味着AT&T将成为美国最大的有线电视运营商，旗下包括包括CNN、TBS、TNT以及产备受人们喜爱的的HBO。美国司法部已决定对这一裁决提起上诉。",
+        imgSrc: "/images/post/AT&T.jpeg",
+        reading: 102,
+				detail: "美国联邦法院法官批准 AT&T 收购时代华纳，驳回去年11月司法部提出的此项收购可能涉嫌垄断的诉讼。之后，AT&T将以850亿美元的价格收购时代华纳，这也意味着AT&T将成为美国最大的有线电视运营商，旗下包括包括CNN、TBS、TNT以及产备受人们喜爱的的HBO。美国司法部已决定对这一裁决提起上诉。\n除了AT&T 收购时代华纳，Comcast也宣布将以600亿美元收购21世纪福克斯旗下的大部分股权。虽然这项收购还未确定，迪士尼也在竞购这家工作室，但可以确定的是，AT&T收购时代华纳的判决，对之后Comcast的收购产生了直接的影响。",
+        collection: 26,
+        dateTime: "24小时前",
+				headImgSrc: "/images/post/AT&T.jpeg",
+        author: "椰子",
+        date: "June 12 2018",
+        avatar: "../../../images/avatar/3.png",
+        postId: 4,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100000Zn0vS4fKKo8.m4a?fromtag=38",
+            title: "沉默是金-张国荣",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000003at0mJ2YrR2H.jpg?max_age=2592000"
+        }
+    },
+    {
+			title: "iPhone或于2019年采用type-c接口",
+			content: "据DigiTimes援引模拟IC供应商消息，苹果计划在 2019 款 iPhone 上实施几项大的变动 ",
+        imgSrc: "/images/post/iPhone.jpeg",
+        reading: 96,
+				detail: "据DigiTimes援引模拟IC供应商消息，苹果计划在 2019 款 iPhone 上实施几项大的变动 —— 除了所有机型从 LCD 屏转向 OLED 屏之外，该公司最终还可能采用 USB-C 端口。DigiTimes 指出，iPhone 和 iPad 都将支持 USB-C，因为该公司正在“重新设计充电器和下一代相关接口”。",
+        collection: 26,
+        dateTime: "21小时前",
+				headImgSrc: "/images/post/iPhone.jpeg",
+        author: "LU",
+        date: "June 12 2018",
+        avatar: "../../../images/avatar/5.png",
+        postId: 5,
+        music: {
+            url: "http://ws.stream.qqmusic.qq.com/C100002I8eGJ28BI17.m4a?fromtag=38",
+            title: "朋友-谭咏麟",
+            coverImg: "http://y.gtimg.cn/music/photo_new/T002R150x150M000004eGsCN3SUheO.jpg?max_age=2592000"
+        }
+    },
+]
+
+module.exports = {
+    postList: local_database
+}
